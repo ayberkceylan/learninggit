937f86e02cffd33f37c87b0a2dadd710c0081934
ayberkceylan <ayberkceylan.12@gmail.com>
2021-03-22 13:26:35 +0300

    Statics

---
+++ static.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+int myfunc(){
+
+static int num  = 4;
+num = num + 5;
+return num;
+}
+*/
+int main(){
+
+/*
+printf("%d ",myfunc());
+printf("%d\n",myfunc()); //A static int variable remains in memory while the program is 			 //running. A normal or auto variable is destroyed when a function call 			 //where the variable was declared is over. 
+
+int x = myfunc();
+int z = myfunc();
+printf("%d\n",x);
+printf("%d\n",z);
+printf("%d\n",x+z);
+*/
+return 0;
+}
+
